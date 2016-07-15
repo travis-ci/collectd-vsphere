3d96518c747f6050fda9e6a3c6bb7fafeedba9ad
Henrik Hodne <henrik@hodne.io>
2016-07-15 19:22:36 +0200

    Split everything into multiple files and types for better overview (#2)
    The behaviour should be the same, but we have tests now so we can verify
    that going forward.

---
+++ stats_collector_test.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"sync"
+	"testing"
+	"time"
+
+	"collectd.org/api"
+)
+
+type fakeAPIWriter struct {
+	metricsMutex sync.Mutex
+	metrics      map[string]api.Value
+}
+
+func (w *fakeAPIWriter) readMetric(metric string) api.Value {
+	w.metricsMutex.Lock()
+	defer w.metricsMutex.Unlock()
+
+	return w.metrics[metric]
+}
+
+func (w *fakeAPIWriter) Write(vl api.ValueList) error {
+	w.metricsMutex.Lock()
+	defer w.metricsMutex.Unlock()
+
+	w.metrics[vl.Identifier.String()] = vl.Values[0]
+
+	return nil
+}
+
+func TestStatsCollector(t *testing.T) {
+	apiWriter := &fakeAPIWriter{metrics: make(map[string]api.Value)}
+
+	collector := NewStatsCollector(apiWriter, time.Millisecond)
+	collector.MarkPowerOnSuccess("on-yes-host")
+	collector.MarkPowerOnSuccess("on-yes-host")
+	collector.MarkPowerOnFailure("on-no-host")
+	collector.MarkPowerOffSuccess("off-yes-host")
+	collector.MarkPowerOffFailure("off-no-host")
+	collector.MarkCloneSuccess("yes-image")
+	collector.MarkCloneFailure("no-image")
+
+	// Sleep for 2 milliseconds to allow the metrics to be written to the
+	// fakeAPIWriter.
+	time.Sleep(2 * time.Millisecond)
+
+	expectedMetrics := []struct {
+		metric string
+		value  api.Value
+	}{
+		{"on-yes-host/vsphere/operations-power_on_success", api.Derive(2)},
+		{"on-yes-host/vsphere/operations-power_on_failure", api.Derive(0)},
+		{"on-yes-host/vsphere/operations-power_off_success", api.Derive(0)},
+		{"on-yes-host/vsphere/operations-power_off_failure", api.Derive(0)},
+
+		{"on-no-host/vsphere/operations-power_on_success", api.Derive(0)},
+		{"on-no-host/vsphere/operations-power_on_failure", api.Derive(1)},
+		{"on-no-host/vsphere/operations-power_off_success", api.Derive(0)},
+		{"on-no-host/vsphere/operations-power_off_failure", api.Derive(0)},
+
+		{"off-yes-host/vsphere/operations-power_on_success", api.Derive(0)},
+		{"off-yes-host/vsphere/operations-power_on_failure", api.Derive(0)},
+		{"off-yes-host/vsphere/operations-power_off_success", api.Derive(1)},
+		{"off-yes-host/vsphere/operations-power_off_failure", api.Derive(0)},
+
+		{"off-no-host/vsphere/operations-power_on_success", api.Derive(0)},
+		{"off-no-host/vsphere/operations-power_on_failure", api.Derive(0)},
+		{"off-no-host/vsphere/operations-power_off_success", api.Derive(0)},
+		{"off-no-host/vsphere/operations-power_off_failure", api.Derive(1)},
+
+		{"yes-image/vsphere/operations-clone_success", api.Derive(1)},
+		{"yes-image/vsphere/operations-clone_failure", api.Derive(0)},
+
+		{"no-image/vsphere/operations-clone_success", api.Derive(0)},
+		{"no-image/vsphere/operations-clone_failure", api.Derive(1)},
+	}
+
+	for _, expected := range expectedMetrics {
+		actualValue := apiWriter.readMetric(expected.metric)
+		if actualValue != expected.value {
+			t.Errorf("expected %s to be %+v, but was %+v", expected.metric, expected.value, actualValue)
+		}
+	}
+}
